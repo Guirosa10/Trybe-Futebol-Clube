8a1962accd1e657e51a0443b756cecf2ee001a08
Guilherme <gui.rsa@hotmail.com>
2022-07-07 14:34:21 -0300

    feat: model users created

---
+++ app/backend/src/database/migrations/20220707170543-create-users.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+    },
+    {
+      timestamps: false,
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
