202fb0ae7874fb46b70c71eb240f8541312b9aff
Guilherme <gui.rsa@hotmail.com>
2022-07-12 19:09:43 -0300

    fix: retirada dos atributos no findAll

--- app/backend/src/controllers/teamsController.ts
+++ app/backend/src/controllers/teamsController.ts
@@ -4,7 +4,7 @@ import teams from '../database/models/teams';
 
 export default class teamsController {
   static async getAll(_req: Request, res: Response) {
-    const results = await teams.findAll({ attributes: ['teamName' as 'team_name'] });
+    const results = await teams.findAll();
     return res.status(200).json(results);
   }
 }
