a47535d1068c72c4d54d79c46808fa0dc84cad46
Guilherme <gui.rsa@hotmail.com>
2022-07-14 01:30:33 -0300

    fix: fix no token

--- app/backend/src/middleware/tokenValidationMiddleware.ts
+++ app/backend/src/middleware/tokenValidationMiddleware.ts
@@ -4,12 +4,17 @@ import * as jwt from 'jsonwebtoken';
 export default class ValidateToken {
   static tokenValidation(req: Request, res: Response, next: NextFunction) {
     console.log('cheguei na validação de token');
-    const token = req.headers.authorization as string;
-    const results = jwt.verify(token, process.env.JWT_SECRET as string);
-    if (!results || results.length < 1) {
+    try {
+      const token = req.headers.authorization as string;
+      const results = jwt.verify(token, process.env.JWT_SECRET as string);
+      console.log(results);
+      if (!results || results === Error) {
+        return res.status(401).json({ message: 'Token must be a valid token' });
+      }
+      console.log('sai da validação de token');
+      next();
+    } catch (error) {
       return res.status(401).json({ message: 'Token must be a valid token' });
     }
-    console.log('sai da validação de token');
-    next();
   }
 }
